1e8c7400298806a4635335f6cc0193e9c2506189
neonlegend <neon@thefireflytech.com>
2026-03-24 09:05:41 +0545

    feat: refactor Trapping Rain Water solution to improve efficiency using a stack-based approach (my solution)

--- leetcodehard/traprainwater.cpp
+++ leetcodehard/traprainwater.cpp
@@ -19,6 +19,10 @@
  * Acceptance: 67.0%
  */
 
+ /**
+  * This is totally my solution
+  */
+
  #include<iostream>
  #include<vector>
  #include<stack>
@@ -28,71 +32,53 @@
 
  class Solution {
 public:
+    struct node {
+        int first = 0;
+        int second = 0;
+        int index = 0;
+    };
+
     int trap(vector<int>& height) {
-        stack<int> increase;
-        stack<int> decrease;
+        int area = 0;
+        stack<node> st;
         int size = height.size();
-        int left = 0, area = 0, start = 0, right = 0;
-        int level = 0, distance = 0, i=0;
+        int dp[size];
+        for (int i=0;i<size;i++) dp[i]=0;
+        int right = 0;
 
+        st.push({height[0], height[0]});
 
-      //   cout<<"[";
-        for (i=0;i<size;i++){
-            if (height[i] != 0){
-                increase.push(height[i]);
-                // cout<<height[i]<<", ";
-                break;
-            }
-        }
-        start = i; left = i;
-        bool trend = true; // increasing or decreasing
-
-        while (left < right || right == 0){
-            trend = true;
-
-            for (right = left+1; right < size; right++){
-                if (trend){
-                    if (increase.empty() || increase.top() <= height[right]){
-                        increase.push(height[right]);
-                        left++;
-                        // cout<<height[right]<<", ";
-                        continue;
-                    } else{
-                        trend = false;
-                        // cout<<endl;
-                    }
-                } 
-               //  cout<<height[right]<<",";
-                decrease.push(height[right]);
-                if (height[right] >= height[left] ){
-                    distance = right - left - 1;
-                    level = min(height[left], height[right]);
-                    area += level * distance;
-                    
-                    left++;
-                    while (left < right){
-                        area -= height[left++];
-                    }
-                    while (!increase.empty()) increase.pop();
-                    increase.push(height[right]);
-                    while (!decrease.empty()) decrease.pop();
-                    trend = true;
-                    // cout<<"]"<<endl;
-                    // cout<<"area = "<<area<<endl;
-                    // cout<<"["<<increase.top()<<", ";
-                }
-            }
+        for (right = 1; right < size; right++){
+            if (height[right] == 0) continue;
             
-            right--; left++;
-            // cout<<"left = "<<left<<endl;
-            // cout<<"right = "<<right<<endl;
-            if (left >= right) break;
-            while (!increase.empty()) increase.pop();
-            increase.push(height[left]);
-            // cout<<"["<<increase.top();
-            while (!decrease.empty()) decrease.pop();
+            if (height[right] < st.top().first){
+                struct node top = st.top();
+                top.second -= height[right];
+                st.pop();
+                st.push(top);
+                int sep = right - st.top().index - 1;
+                dp[right] = height[right] * sep;
+                st.push({height[right], height[right], right});
+                continue;
+            }
+
+            int tempSecond = height[right];
+            while (!st.empty() && st.top().first <= height[right]){
+                int sep = right - st.top().index - 1;
+                dp[right] += st.top().second * sep;
+                tempSecond = st.top().first;
+                st.pop();
+            }
+
+            if (!st.empty()){
+                st.top().second = st.top().first - height[right];
+                dp[right] += (height[right] - tempSecond) * (right - st.top().index - 1);
+            }
+            st.push({height[right], height[right], right});
         }
 
+        area = 0;
+        for (int i=0;i<size;i++) area += dp[i];
         return area;        
     }
 };
@@ -113,31 +99,44 @@ int main() {
       [2, 0, 2]
       [3, 0, 0, 2, 0, 4]
       [5, 4, 1, 2, 3, 5]
+
+      [4, 2, 3]
+      [5, 0, 1, 0, 3]
+      [6, 0, 2, 0, 5]
+      [10, 0, 1, 0, 2, 0, 3]
+
+      [3, 1, 2, 1, 3]
+      [5, 0, 1, 0, 4]
+
+      [4,0,3,0,2,0,5]
+      [8,3,1,2,1,4,1,6]
+      [0,3,0,1,0,2,0,5,0]
+      [2,6,3,5,2,8,1,4,2,2,5,3,2]
     */
    
    Solution s; 
    vector<int> height;
    
-   height = {0,1,0,2,1,0,1,3,2,1,2,1};
-   cout<<s.trap(height)<<endl;
+//    height = {0,1,0,2,1,0,1,3,2,1,2,1};
+//    cout<<s.trap(height)<<endl;
 
-   height = {4,2,0,3,2,5};
-   cout<<s.trap(height)<<endl;
+   // height = {4,2,0,3,2,5};
+   // cout<<s.trap(height)<<endl;
 
-   height = {0,1,0};
-   cout<<s.trap(height)<<endl;
+   // height = {0,1,0};
+   // cout<<s.trap(height)<<endl;
 
-   height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-   cout<<s.trap(height)<<endl;
+   // height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+   // cout<<s.trap(height)<<endl;
 
-   height = {0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3};
-   cout<<s.trap(height)<<endl;
+//    height = {0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3};
+//    cout<<s.trap(height)<<endl;
 
-   height = {3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 1};
-   cout<<s.trap(height)<<endl;
+   // height = {3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 1};
+   // cout<<s.trap(height)<<endl;
 
-   height = {0, 0, 1, 0, 1, 2, 1, 0, 0, 1, 0, 0};
-   cout<<s.trap(height)<<endl;
+//    height = {0, 0, 1, 0, 1, 2, 1, 0, 0, 1, 0, 0};
+//    cout<<s.trap(height)<<endl;
 
    // height = {1, 2, 3, 4};
    // cout<<s.trap(height)<<endl;
@@ -153,6 +152,33 @@ int main() {
 
    // height = {5, 4, 1, 2, 3, 5};
    // cout<<s.trap(height)<<endl;
+
+   // height = {3, 1, 2, 1, 3};
+   // cout<<s.trap(height)<<endl;
+
+   // height = {5, 0, 1, 0, 4};
+   // cout<<s.trap(height)<<endl;
+
+   // height = {4,0,3,0,2,0,5};
+   // cout<<s.trap(height)<<endl;
+
+   // height = {8,3,1,2,1,4,1,6};
+   // cout<<s.trap(height)<<endl;
+
+//    height = {0,3,0,1,0,2,0,5,0};
+//    cout<<s.trap(height)<<endl;
+
+//    height = {2,6,3,5,2,8,1,4,2,2,5,3,2,3};
+//    cout<<s.trap(height)<<endl;
+
+    // height = {2,6,3,5,2,8,1,4};
+    // cout<<s.trap(height)<<endl;
+
+    // height = {5, 4, 3, 2, 1, 2, 3, 4, 5};
+    // cout<<s.trap(height)<<endl;
+
+    height = {5, 4, 5, 4, 5};
+    cout<<s.trap(height)<<endl;
 
    return 0;
  }
