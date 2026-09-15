f9b9ba234001d2771b02271b0fa7afbd57661ef9
Neon17 <neon.neupane.9@gmail.com>
2026-09-15 07:28:40 +0545

    feat: enhance solution for Leetcode Problem 139 - Word Break with dynamic programming approach

--- dynamic_programming/WordBreak.cpp
+++ dynamic_programming/WordBreak.cpp
@@ -17,11 +17,13 @@ Constraints:
 #include <vector>
 #include <string>
 #include <unordered_set>
+#include <climits>
 using namespace std;
 
 class Solution {
     vector<vector<int>> dp;
-    bool backtrack(string s, unordered_set<string>& dict, int index = 0, string curr=""){
+    vector<int> dps;
+    bool backtrack(string& s, unordered_set<string>& dict, int index = 0, string curr=""){
         if (index == s.size()) return dict.count(curr);
         curr += s[index];
         int start = index - curr.size() + 1;
@@ -33,12 +35,45 @@ class Solution {
         if (curr.size() <= 20) b = backtrack(s, dict, index+1, curr);
         return dp[start][index] = a||b;
     }
+    bool standardBacktrack(string& s, unordered_set<string>& dict, int index = 0){
+        if (index == s.size()) return true;
+        if (dps[index] != -1) return dps[index];
+
+        for (const string& word: dict){
+            int len = word.size();
+            if (index + len <= s.size() &&  !s.compare(index, len, word)) {
+                if (standardBacktrack(s, dict, index+len)) return dps[index] = 1;
+            }
+        }        
+        return false;
+    }
+    bool convertToDP(string& s, unordered_set<string>& dict){
+        int size = s.size();
+        vector<char>dpd(size+1, 0);
+        dpd[0] = 1;
+
+        for (int i=0;i<size;i++) {
+            if (!dpd[i]) continue;
+            
+            for (const string& word: dict){
+                int len = word.size();
+                if (i + len <= size && !s.compare(i, len, word)){
+                    dpd[i+len] = 1;
+                    if ( (i + len) == size ) return true;
+                }
+            }
+        }
+        return dpd[size];
+    }
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string> dict;
-        dp.assign(s.size(), vector<int>(s.size(), -1));
-        for (int i=0;i<wordDict.size();i++) dict.insert(wordDict[i]);
-        return backtrack(s, dict);
+        // dp.assign(s.size(), vector<int>(s.size(), -1));
+        // dps.assign(s.size(), -1);
+        // for (int i=0;i<wordDict.size();i++) dict.insert(wordDict[i]);
+        // return backtrack(s, dict);
+        // return standardBacktrack(s, dict);
+        return convertToDP(s, dict);
     }
 };
 
